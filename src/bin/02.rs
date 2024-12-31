8756ef1a66ba42b3690afe2fd307b9c9530551c0
Stathis Stamoulis <sstamoulis@mosystems.biz>
2024-12-31 15:45:54 +0200

    Solve day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,16 +13,55 @@ fn read_to_lists(input: &str) -> Option<Vec<Vec<u64>>> {
         .collect()
 }
 
+fn get_permutations<'a>(
+    list: &'a [u64],
+) -> impl Iterator<Item = impl Iterator<Item = u64> + use<'a>> {
+    (0..list.len()).map(move |n| {
+        list.iter()
+            .enumerate()
+            .filter_map(move |(i, x)| (i != n).then_some(*x))
+    })
+}
+
+fn is_safe(list: &[u64]) -> bool {
+    let diff = list
+        .windows(2)
+        .map(|w| match w {
+            &[a, b] => Some((b as i64).sub(a as i64)),
+            _ => None,
+        })
+        .collect::<Option<Vec<_>>>()
+        .unwrap();
+    let increasing = diff.iter().all(|&x| x >= 0);
+    let decreasing = diff.iter().all(|&x| x <= 0);
+    if increasing || decreasing {
+        let stepped = diff.into_iter().all(|x| {
+            let x = x.abs();
+            x >= 1 && x <= 3
+        });
+        stepped
+    } else {
+        false
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let lists = read_to_lists(input)?;
-    let results = lists.iter().map(|list| {
-        list.windows(2).map(|[a,b]|i64::sub(*b, *a))
-    }).collect();
-    None
+    let results = lists.iter().map(|list| is_safe(list)).collect::<Vec<_>>();
+    // println!("{:#?}", results);
+    Some(results.into_iter().filter(|&x| x).count() as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let lists = read_to_lists(input)?;
+    let results = lists
+        .iter()
+        .map(|list| {
+            is_safe(list) || get_permutations(list).any(|iter| is_safe(&iter.collect::<Vec<_>>()))
+        })
+        .collect::<Vec<_>>();
+    // println!("{:#?}", results);
+    Some(results.into_iter().filter(|&x| x).count() as u64)
 }
 
 #[cfg(test)]
@@ -32,12 +71,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
